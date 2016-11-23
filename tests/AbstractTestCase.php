952d5a4c11df87a00007deec237b6e0c222b2604
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-23 13:38:13 +0200

    Applied fixes from StyleCI (#2)
    [ci skip] [skip ci]

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Collection Export.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Tests\CollectionExport;
 
 use BrianFaust\CollectionExport\ServiceProvider;
